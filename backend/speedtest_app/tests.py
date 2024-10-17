68009d549e75dd96d4edf02eac02f891463a4713
soulcloud <solomon.anamm@gmail.com>
2024-10-17 16:18:12 +0100

    added logging

--- backend/speedtest_app/tests.py
+++ backend/speedtest_app/tests.py
@@ -1,3 +1,16 @@
+# speedtest_app/tests.py
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class SpeedTestViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_speedtest_endpoint(self):
+        response = self.client.get('/api/speedtest/test/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('download', response.data)
+        self.assertIn('upload', response.data)
+        self.assertIn('ping', response.data)
+        self.assertIn('server', response.data)
