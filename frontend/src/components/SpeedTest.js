e3ce41a298cc9784d3f098de303dce4bb15afb2b
soulcloud <solomon.anamm@gmail.com>
2024-10-29 23:33:32 +0100

    change layout of frontend

--- frontend/src/components/SpeedTest.js
+++ frontend/src/components/SpeedTest.js
@@ -1,4 +1,3 @@
-// src/components/SpeedTest.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -11,10 +10,15 @@ const SpeedTest = () => {
     setLoading(true);
     setResult(null);
     setError(null);
+    const apiUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';  // Fallback URL
+    
     try {
-      const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/speedtest/test/`);
-
+      const response = await axios.get(`${apiUrl}/speedtest/test/`);
+      console.log("Response data:", response.data);  // Check the response structure
+      setResult(response.data);  // Set the result with the response data
+      
     } catch (err) {
+      console.error("Error performing speed test:", err);
       setError('Failed to perform speed test.');
     } finally {
       setLoading(false);
@@ -43,5 +47,3 @@ const SpeedTest = () => {
 };
 
 export default SpeedTest;
-
-
